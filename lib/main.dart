9523aadb8fbd46d9664c9eaa57b2444826e486e6
altayhunoglu <muratfair@gmail.com>
2020-10-20 19:02:31 +0300

    operatorler eklendi.

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   String _currentNumber = "";
+  String _operator = "";
 
   void _clickNumber(String number) {
     setState(() {
@@ -40,8 +41,49 @@ class _MyHomePageState extends State<MyHomePage> {
   void _clickCE() {
     print("CE'ye basti");
   }
+  void _clickEqual() {
+    List<String> listOfNumbers = _currentNumber.split(_operator);
+
+    double a = double.parse(listOfNumbers[0]);
+    double b = double.parse(listOfNumbers[1]);
+
+    double result = 0;
+
+    switch(_operator){
+      case "-":
+         result = a - b;
+         break;
+      case "+":
+        result = a + b;
+        break;
+      case "*":
+        result = a * b;
+        break;
+      case ":":
+        result = a / b;
+        break;
+    }
+    setState(() {
+      _currentNumber =  result.toString();
+    });
+
+  }
   void _clickC() {
     print("C'ye basti");
+    setState(() {
+
+      _currentNumber = "";
+
+    });
+  }
+
+  void _chooseOperator(String operator) {
+    print(operator + " basıldı");
+    setState(() {
+      _operator = operator;
+      _currentNumber = _currentNumber + operator;
+
+    });
   }
   @override
   Widget build(BuildContext context) {
@@ -52,125 +94,176 @@ class _MyHomePageState extends State<MyHomePage> {
 
         title: Text(widget.title),
       ),
-      body: Center(
+      body:   Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
 
-        child: Column(
+        children: [
+          getNumbers(),
+          getOperators()
+        ],)
 
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+      );
+  }
 
-            Text(
-              _currentNumber,
-              style: TextStyle(color: CupertinoColors.white, fontSize: 27),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-              FlatButton(
-                    onPressed: () {
-                           _clickNumber("1");
-                    },
-                    child: Text("1", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
+  Widget getNumbers() {
+    return Column(
 
-                      ),  color: Colors.blue,),
+      children: <Widget>[
 
-              FlatButton(
-                onPressed: () {
-                  _clickNumber("2");
-                },
-                child: Text("2", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
-
-                ),  color: Colors.blue,),
-                FlatButton(
-                  onPressed: () {
-                    _clickNumber("3");
-                  },
-                  child: Text("3", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
-
-                  ),  color: Colors.blue,)
-            ],),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                FlatButton(
-                  onPressed: () {
-                    _clickNumber("4");
-                  },
-                  child: Text("4", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
-
-                  ),  color: Colors.blue,),
-
-                FlatButton(
-                  onPressed: () {
-                    _clickNumber("5");
-                  },
-                  child: Text("5", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
-
-                  ),  color: Colors.blue,),
-                FlatButton(
-                  onPressed: () {
-                    _clickNumber("6");
-                  },
-                  child: Text("6", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
-
-                  ),  color: Colors.blue,)
-              ],),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                FlatButton(
-                  onPressed: () {
-                    _clickNumber("7");
-                  },
-                  child: Text("7", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
-
-                  ),  color: Colors.blue,),
-
-                FlatButton(
-                  onPressed: () {
-                    _clickNumber("8");
-                  },
-                  child: Text("8", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
-
-                  ),  color: Colors.blue,),
-                FlatButton(
-                  onPressed: () {
-                    _clickNumber("9");
-                  },
-                  child: Text("9", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
-
-                  ),  color: Colors.blue,)
-              ],),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                FlatButton(
-                  onPressed: () {
-                    _clickC();
-                  },
-                  child: Text("C", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
-
-                  ),  color: Colors.blue,),
-
-                FlatButton(
-                  onPressed: () {
-                    _clickNumber("0");
-                  },
-                  child: Text("0", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
-
-                  ),  color: Colors.blue,),
-                FlatButton(
-                  onPressed: () {
-                    _clickCE();
-                  },
-                  child: Text("CE", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
-
-                  ),  color: Colors.blue,)
-              ],)
-
-          ],
+        Text(
+          _currentNumber,
+          style: TextStyle(color: CupertinoColors.white, fontSize: 27),
         ),
-      ) // This trailing comma makes auto-formatting nicer for build methods.
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            FlatButton(
+              onPressed: () {
+                _clickNumber("1");
+              },
+              child: Text("1", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
+
+              ),  color: Colors.blue,),
+
+            FlatButton(
+              onPressed: () {
+                _clickNumber("2");
+              },
+              child: Text("2", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
+
+              ),  color: Colors.blue,),
+            FlatButton(
+              onPressed: () {
+                _clickNumber("3");
+              },
+              child: Text("3", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
+
+              ),  color: Colors.blue,)
+          ],),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            FlatButton(
+              onPressed: () {
+                _clickNumber("4");
+              },
+              child: Text("4", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
+
+              ),  color: Colors.blue,),
+
+            FlatButton(
+              onPressed: () {
+                _clickNumber("5");
+              },
+              child: Text("5", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
+
+              ),  color: Colors.blue,),
+            FlatButton(
+              onPressed: () {
+                _clickNumber("6");
+              },
+              child: Text("6", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
+
+              ),  color: Colors.blue,)
+          ],),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            FlatButton(
+              onPressed: () {
+                _clickNumber("7");
+              },
+              child: Text("7", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
+
+              ),  color: Colors.blue,),
+
+            FlatButton(
+              onPressed: () {
+                _clickNumber("8");
+              },
+              child: Text("8", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
+
+              ),  color: Colors.blue,),
+            FlatButton(
+              onPressed: () {
+                _clickNumber("9");
+              },
+              child: Text("9", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
+
+              ),  color: Colors.blue,)
+          ],),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            FlatButton(
+              onPressed: () {
+                _clickC();
+              },
+              child: Text("C", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
+
+              ),  color: Colors.blue,),
+
+            FlatButton(
+              onPressed: () {
+                _clickNumber("0");
+              },
+              child: Text("0", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
+
+              ),  color: Colors.blue,),
+            FlatButton(
+              onPressed: () {
+                _clickCE();
+              },
+              child: Text("CE", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
+
+              ),  color: Colors.blue,),
+
+          ],),
+
+      ],
     );
+  }
+  Widget getOperators() {
+    return    Column(children: [
+      FlatButton(
+        onPressed: () {
+          _chooseOperator("-");
+        },
+        child: Text("-", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
+
+        ),  color: Colors.blue,),
+
+      FlatButton(
+        onPressed: () {
+          _chooseOperator("+");
+        },
+        child: Text("+", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
+
+        ),  color: Colors.blue,),
+
+      FlatButton(
+        onPressed: () {
+          _chooseOperator("*");
+        },
+        child: Text("*", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
+
+        ),  color: Colors.blue,),
+      FlatButton(
+        onPressed: () {
+          _chooseOperator(":");
+        },
+        child: Text(":", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
+
+        ),  color: Colors.blue,),
+
+      FlatButton(
+        onPressed: () {
+          _clickEqual();
+        },
+        child: Text("=", style: TextStyle(color: CupertinoColors.white, fontSize: 27),
+
+        ),  color: Colors.blue,),
+
+    ],);
   }
 }
